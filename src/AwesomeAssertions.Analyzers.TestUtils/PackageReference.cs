23841cc5979b52680659feca2a3ccc597a9f88cf
Christoph Bersch <2921215+cbersch@users.noreply.github.com>
2025-05-26 19:42:28 +0200

    Update AwesomeAssertions reference to 9.0.0

--- src/AwesomeAssertions.Analyzers.TestUtils/PackageReference.cs
+++ src/AwesomeAssertions.Analyzers.TestUtils/PackageReference.cs
@@ -18,7 +18,7 @@ public class PackageReference
         _allDependencies.Add(this);
     }
 
-    public static PackageReference AwesomeAssertions_latest { get; } = new("AwesomeAssertions", "9.0.0-rc.1", "lib/netstandard2.0/");
+    public static PackageReference AwesomeAssertions_latest { get; } = new("AwesomeAssertions", "9.0.0", "lib/netstandard2.0/");
     public static PackageReference MSTestTestFramework_3_1_1 { get; } = new("MSTest.TestFramework", "3.1.1", "lib/netstandard2.0/");
     public static PackageReference XunitAssert_2_5_1 { get; } = new("xunit.assert", "2.5.1", "lib/netstandard1.1/");
     public static PackageReference Nunit_3_14_0 { get; } = new("NUnit", "3.14.0", "lib/netstandard2.0/");
